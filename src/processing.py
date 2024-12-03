3e35a80417bbff50d7a863206e1c390fa5e202f0
Alexander Averin <alexaverin201156@gmail.com>
2024-12-03 17:40:40 +0300

    func sort_by_date() added

--- src/processing.py
+++ src/processing.py
@@ -3,6 +3,11 @@ def filter_by_state(data_in: list[dict], state: str = 'EXECUTED') -> list[dict]:
     return [d for d in data_in if d.get('state', '') == state]
 
 
+def sort_by_date(data_in: list[dict], desc_order: bool = True) -> list[dict]:
+    """ функция возвращает список отсортированный по указанному порядку по ключу date """
+    return sorted(data_in, reverse=desc_order, key=lambda x: x['date'])
+
+
 if __name__ == '__main__':
     test_data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
@@ -14,4 +19,3 @@ if __name__ == '__main__':
     print(filter_by_state(test_data))
     print('CANCELED')
     print(filter_by_state(test_data, 'CANCELED'))
-
