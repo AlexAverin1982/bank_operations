42eb9074764314cb09d13e7655270550ea3852b1
Alexander Averin <alexaverin201156@gmail.com>
2024-12-16 21:41:05 +0300

    init 11_1

---
+++ src/generators.py
@@ -0,0 +1,9 @@
+def filter_by_currency(data: list[dict]):
+    pass
+
+
+def transaction_descriptions(data: list[dict]):
+    pass
+
+def card_number_generator(operation: str):
+    pass
---
+++ tests/test_generators.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_filter_by_currency():
+    assert True
+
+
+def test_transaction_descriptions():
+    assert True
+
+
+def test_card_number_generator():
+    assert True
