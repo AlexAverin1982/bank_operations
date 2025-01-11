bfbbf769bba5ba21ddf4af53f5c2426ebf41ca24
Alexander Averin <alexaverin201156@gmail.com>
2025-01-11 18:25:51 +0300

    12_2 utils logging tested (cp1251 in log files?)

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,37 @@
+import logging
+import os.path
+
+par_dir = os.path.abspath(os.path.join(__file__, os.pardir))
+par_dir = os.path.abspath(os.path.join(par_dir, os.pardir))
+masks_log_filename = os.path.join(par_dir, "logs", "masks.log")
+
+# Основная конфигурация logging
+logging.basicConfig(level=logging.DEBUG, filemode="w")
+masks_logger = logging.getLogger("masks_logger")
+masks_logger.setLevel(logging.DEBUG)
+masks_log_handler = logging.FileHandler(filename=masks_log_filename)
+""" Формат записи логов включает метку времени, название модуля, уровень серьезности и сообщение """
+masks_log_formatter = logging.Formatter(
+    "%(asctime)s %(levelname)s in module %(filename)s: %(message)s"
+)
+masks_log_handler.setFormatter(masks_log_formatter)
+masks_logger.addHandler(masks_log_handler)
+
+
 def get_mask_card_number(card_number: int) -> str:
     """функция принимает на вход номер банковской карты в виде числа и возвращает строку с замаскированным номером"""
     if not isinstance(card_number, int):
+        masks_logger.error(
+            f"{card_number} не является номером карты, не состоит из 16 цифр"
+        )
         raise TypeError("Номер карты - 16 цифр")
     if len(str(card_number)) != 16:
+        masks_logger.error(
+            f"номер карты {card_number} содержит меньше или больше 16 цифр"
+        )
         raise ValueError("Номер карты - 16 цифр")
 
+    masks_logger.info("номер карты замаскирован успешно")
     return (
         str(card_number)[:4]
         + " "
@@ -17,7 +44,14 @@ def get_mask_card_number(card_number: int) -> str:
 def get_mask_account(account_no: int) -> str:
     """функция принимает на вход номер банковского счета в виде числа и возвращает строку с замаскированным номером"""
     if not isinstance(account_no, int):
+        masks_logger.error(
+            f"{account_no} не является номером счета, не состоит из 20 цифр"
+        )
         raise TypeError("Номер счета - 20 цифр")
     if len(str(account_no)) != 20:
+        masks_logger.error(
+            f"номер счета {account_no} содержит меньше или больше 20 цифр"
+        )
         raise ValueError("Номер счета - 20 цифр")
+    masks_logger.info("номер счета замаскирован успешно")
     return "**" + str(account_no)[-4:]
--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,23 @@
 import json
-import os.path
 import logging
+import os.path
 
 from src.external_api import convert_currencies
 
 par_dir = os.path.abspath(os.path.join(__file__, os.pardir))
 par_dir = os.path.abspath(os.path.join(par_dir, os.pardir))
-templatedir = os.path.join(par_dir, "data", "templates")
-reportsdir = os.path.join(par_dir, "data", "reports")
+utils_log_filename = os.path.join(par_dir, "logs", "utils.log")
 
-utils_logger = logging.getLogger('utils_logger')
+logging.basicConfig(level=logging.DEBUG, filemode="w", encoding="utf-8")
+utils_logger = logging.getLogger("utils_logger")
 utils_logger.setLevel(logging.DEBUG)
-utils_log_handler = logging.FileHandler(filename=filename)
+utils_log_handler = logging.FileHandler(filename=utils_log_filename, encoding="utf-8")
+""" Формат записи логов включает метку времени, название модуля, уровень серьезности и сообщение. """
+utils_log_formatter = logging.Formatter(
+    "%(asctime)s %(levelname)s in module %(filename)s: %(message)s"
+)
+utils_log_handler.setFormatter(utils_log_formatter)
+utils_logger.addHandler(utils_log_handler)
 
 
 def load_ops_from_json_file(filename: str) -> list[dict]:
@@ -21,8 +27,12 @@ def load_ops_from_json_file(filename: str) -> list[dict]:
         try:
             with open(filename, encoding="utf-8") as f:
                 result = json.load(f)
-        except json.JSONDecodeError:
+            utils_logger.info(f"файл {filename} с данными операций загружен успешно")
+        except json.JSONDecodeError as ex:
             result = []
+            utils_logger.error(ex)
+    else:
+        utils_logger.error(f"файл {filename} не найден")
     return result
 
 
@@ -31,6 +41,7 @@ def transaction_amount(transaction: dict, target_cur_code: str = "RUB") -> float
     в рублях (по умолчанию) или в другой указанной валюте"""
     op_amount = transaction.get("operationAmount", {})
     if op_amount:
+        utils_logger.info("данные о транзакции загружены успешно")
         currency = op_amount.get("currency", {})
         amount = op_amount.get("amount")
         if amount:
@@ -40,16 +51,25 @@ def transaction_amount(transaction: dict, target_cur_code: str = "RUB") -> float
                 if cur_code != target_cur_code:
                     result = convert_currencies(cur_code, target_cur_code, amount)
                     if result != 0:
+                        utils_logger.info(
+                            f"валюта {cur_code} переконвертирована в {target_cur_code} успешно"
+                        )
                         return result
                     else:
+                        utils_logger.error("Код валюты транзакции указан неверно")
                         raise ValueError("Код валюты транзакции указан неверно")
                 else:
+                    utils_logger.info("размер транзакции в рублях определен успешно")
                     return float(amount)
             else:
+                utils_logger.error("данные о валюте не найдены")
                 raise ValueError("Транзакция содержит неполные данные")
         else:
+            utils_logger.error("данные о транзакции в валюте не найдены")
             raise ValueError("Транзакция содержит неполные данные")
     else:
+        utils_logger.error("данные о размере транзакции не найдены")
         raise ValueError("Транзакция содержит неполные данные")
+
 
 # d = load_ops_from_json_file(u'..\data\operations.json')
